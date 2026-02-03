d9629ca2649e31ea96c04a76c043d425a7888017
Zhiwei Li <lzwjava@gmail.com>
2026-02-04 02:50:50 +0800

    fix(clash): remove '香港' from default proxy filter list

--- scripts/clash/clash.py
+++ scripts/clash/clash.py
@@ -241,7 +241,7 @@ def main():
                 name_filter = ["SG", "TW"]
                 filter_desc = "SG/TW"
             else:
-                name_filter = ["新加坡", "台湾", "日本", "美国", "香港", "印度", "越南", "加拿大"]
+                name_filter = ["新加坡", "台湾", "日本", "美国", "印度", "越南", "加拿大"]
                 filter_desc = "SG/TW/JP/US"
 
             logging.info("Testing proxy speeds to find the best one...")
